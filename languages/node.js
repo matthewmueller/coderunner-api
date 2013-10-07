55b88e40e9ce8193b5498a7f047865615a8bde79
Matthew Mueller <mattmuelle@gmail.com>
2013-10-06 17:16:24 -0700

    added core libraries

--- languages/node.js
+++ languages/node.js
@@ -9,3 +9,16 @@ exports.install = 'npm install -s'
  */
 
 exports.run = 'node .';
+
+/**
+ * Core libraries
+ *
+ * Probably would be handled on client-side. Core libraries shouldn't be in
+ * the package.json anyway.
+ */
+
+// exports.core = [ 'assert', 'buffer', 'child_process', 'cluster', 'crypto', 'dgram', 'dns',
+//                  'domain', 'events', 'fs', 'http', 'https', 'net', 'os', 'path', 'punycode',
+//                  'querystring', 'readline', 'stream', 'string_decoder', 'tls', 'tty', 'url',
+//                  'util', 'vm', 'zlib', 'smalloc'
+//                ];
